2161ae3f9a415fad8dbfc3d55090b3a841158c5e
bricktrooper <kylepinto@yahoo.ca>
2021-06-24 21:21:42 -0400

    adder: iterate through all input combinations

--- adder/adder_tb.v
+++ adder/adder_tb.v
@@ -2,27 +2,37 @@ module AdderTB;
 
 	parameter DELAY = 1;
 
-	reg augend;
-	reg addend;
-	wire sum;
-	wire carry;
+	reg i_augend;
+	reg i_addend;
+	wire o_sum;
+	wire o_carry;
 
 	Adder adder
 	(
-		.i_augend(augend),
-		.i_addend(addend),
-		.o_sum(sum),
-		.o_carry(carry)
+		.i_augend(i_augend),
+		.i_addend(i_addend),
+		.o_sum(o_sum),
+		.o_carry(o_carry)
 	);
+
+	integer expected;
+	integer actual;
 
 	initial begin
 		$dumpfile("adder.vcd");
-		$dumpvars();
+		$dumpvars(0, adder);
 
-		# DELAY;
-		augend = 1;
-		addend = 1;
-		# DELAY;
+		for (integer x = 0; x < 2; x++)begin
+			for (integer y = 0; y < 2; y++) begin
+				i_augend = x;
+				i_addend = y;
+				# DELAY;
+				expected = x + y;
+				actual = {o_carry, o_sum};
+				if (expected != actual)
+					$display("[%0d + %0d]: Expected %0d but result was %0d", x, y, expected, actual);
+			end
+		end
 
 		$finish;
 	end

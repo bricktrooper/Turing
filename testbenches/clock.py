854bc0633a2ec9ec4f351df0a11771ac7da0c42d
bricktrooper <kylepinto@yahoo.ca>
2021-07-03 16:18:09 -0400

    clock: Clock generator script

---
+++ testbenches/clock.py
@@ -0,0 +1,48 @@
+import log
+import cocotb
+
+from cocotb.triggers import RisingEdge
+from cocotb.triggers import Timer
+
+class Clock:
+
+	TIME_UNIT = "ns"
+
+	def __init__(self, signal, period):
+		self.signal = signal
+		self.period = period
+		self.frequency =  1000 / float(period)
+		self.process = None
+
+	def reset(self):
+		self.signal <= 0
+
+	async def next(self, cycles = 1):
+		for i in range(cycles):
+			await RisingEdge(self.signal)
+
+	async def run(self):
+		while True:
+			self.signal <= 1
+			await Timer(self.period / 2, "ns")
+			self.signal <= 0
+			await Timer(self.period / 2, "ns")
+
+	def start(self):
+		if self.process != None:
+			log.error("The clock is already running")
+		else:
+			self.process = cocotb.fork(self.run())
+
+	def stop(self):
+		if self.process == None:
+			log.error("The clock has already stopped")
+		else:
+			self.process.kill()
+			self.process = None
+
+	def print(self):
+		log.info("======= CLOCK =======")
+		log.info("Period:    %.1f ns" % (self.period))
+		log.info("Frequency: %.1f MHz" % (self.frequency))
+		log.info("=====================")

7861d250d35fffea54a083af4f2735d037f4b899
bricktrooper <kylepinto@yahoo.ca>
2021-07-07 17:00:45 -0400

    adder: Print input and output values when a mismatch is detected

--- testbenches/adder/adder.py
+++ testbenches/adder/adder.py
@@ -5,36 +5,39 @@ from math import pow
 from clock import Clock
 from cocotb.binary import BinaryValue
 
-def join(carry, sum):
+def join(sum, carry):
 	BITS = sum.n_bits
 	value = BinaryValue(n_bits = BITS + 1,
 	                    bigEndian = False,
 						value = sum.integer + (carry.integer << BITS))
-	return value
+	return value.integer
 
 async def sweep(dut, clock):
 	clock.reset()
 	N = dut.N
 	VALUES = int(pow(2, N))
 
-	for x in range(VALUES):
-		for y in range(VALUES):
-			dut.augend <= x
-			dut.addend <= y
+	for augend in range(VALUES):
+		for addend in range(VALUES):
+			dut.augend <= augend
+			dut.addend <= addend
 			await clock.next()
 
-			expected = x + y
-			actual = join(dut.carry.value, dut.sum.value)
+			expected = augend + addend
+			actual = join(dut.sum.value, dut.carry.value)
 
 			sum = dut.sum.value.integer
 			carry = dut.carry.value.integer
 
 			if actual != expected:
-				log.error("[sum = %u, carry = %u]: %u + %u != %u" % (sum, carry, x, y, actual))
+				log.error(f"{augend} + {addend} != {actual}")
+				log.info("augend: %u" % (augend))
+				log.info("augend: %u" % (addend))
+				log.info("sum:    %u" % (sum))
+				log.info("carry:  %u" % (carry))
 				return
 
-			log.success("[sum = %u, carry = %u]: %u + %u == %u" % (sum, carry, x, y, expected))
-
+			log.success(f"{augend} + {addend} = {expected}")
 
 @cocotb.test()
 async def testbench(dut):

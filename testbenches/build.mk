1be865ab68d83f008365af33854d203a5a692058
bricktrooper <kylepinto@yahoo.ca>
2021-08-17 21:36:15 -0400

    build: Simplify waves clean

--- testbenches/build.mk
+++ testbenches/build.mk
@@ -1,11 +1,11 @@
 include config.mk
 
-ifeq ($(SRC),)
-$(error 'SRC' was not specified)
-else ifeq ($(DUT),)
-$(error 'DUT' was not specified)
-else ifeq ($(TB),)
-$(error 'TB' was not specified)
+ifeq ($(SRC), )
+	$(error 'SRC' was not specified)
+else ifeq ($(DUT), )
+	$(error 'DUT' was not specified)
+else ifeq ($(TB), )
+	$(error 'TB' was not specified)
 endif
 
 FLAGS += -Wanachronisms
@@ -23,15 +23,16 @@ FLAGS += -Wsensitivity-entire-array
 
 FLAGS += -g2005
 
-ifeq ($(WAVES),1)
-FLAGS += -D WAVES
-endif
-
 ART += sim_build
 ART += results.xml
 ART += __pycache__
 ART += ../__pycache__
 ART += *.out
+
+ifeq ($(WAVES), 1)
+	FLAGS += -D WAVES
+	ART += *.vcd
+endif
 
 SIM ?= icarus
 TOPLEVEL_LANG ?= verilog
@@ -48,8 +49,6 @@ include $(shell cocotb-config --makefiles)/Makefile.sim
 
 simulate: sim
 
-waves: simulate
-
 compile: $(SRC)
 	@for source in $(SRC); do echo "IVERILOG $$source"; done
 	@iverilog $(FLAGS) -o sim.out $^
@@ -57,7 +56,3 @@ compile: $(SRC)
 clean::
 	@for artifact in $(ART); do echo "RM $$artifact"; done
 	@rm -rf $(ART)
-
-clean-waves: clean
-	@echo "RM *.vcd"
-	@rm -rf *.vcd

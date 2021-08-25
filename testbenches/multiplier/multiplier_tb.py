6167bf5345c757e5b1c8b763e39a5084bf0faa4c
bricktrooper <kylepinto@yahoo.ca>
2021-08-24 21:31:15 -0400

    multiplier: Refactor testbench

--- testbenches/multiplier/multiplier_tb.py
+++ testbenches/multiplier/multiplier_tb.py
@@ -1,26 +1,55 @@
 import log
 import cocotb
 
-from math import pow, prod
 from clock import Clock
-from cocotb.binary import BinaryValue
 
-def calculate_product(N, multiplicand, multiplier):
-	product = BinaryValue(n_bits = 2 * N, value = multiplicand * multiplier, bigEndian = False)
-	overflow = int(product[(2 * N) - 1 : N] != 0)
-	product = product[N - 1 : 0].integer
+def predict_output(N, multiplicand, multiplier):
+	if multiplicand * multiplier > (2 ** N) - 1:
+		product = (multiplicand * multiplier) % (2 ** N)
+		overflow = 1
+	else:
+		product = multiplicand * multiplier
+		overflow = 0
 	return (product, overflow)
 
-def print_io(multiplicand, multiplier, product, overflow):
-	log.info(f"multiplicand : {multiplicand}")
-	log.info(f"multiplier   : {multiplier}")
-	log.info(f"product      : {product}")
-	log.info(f"overflow     : {overflow}")
+def print_io(N, multiplicand, multiplier, product, overflow):
+	log.info("============== I/O ==============")
+	log.info(f"N              : {N} b")
+	log.info(f"i_multiplicand : {multiplicand}")
+	log.info(f"i_multiplier   : {multiplier}")
+	log.info(f"o_product      : {product}")
+	log.info(f"o_overflow     : {overflow}")
+	log.info("=================================")
+
+def verify(N, cycles, multiplicand, multiplier, product, overflow):
+	(expected_product, expected_overflow) = predict_output(N, multiplicand, multiplier)
+
+	overflow_string = None
+	if overflow == 1:
+		overflow_string = "with overflow"
+	else:
+		overflow_string = "without overflow"
+
+	if product != expected_product:
+		log.error(f"{multiplicand} * {multiplier} != {product} {overflow_string}")
+		print_io(N, multiplicand, multiplier, product, overflow)
+		exit(-1)
+
+	if overflow != expected_overflow:
+		log.error(f"{multiplicand} * {multiplier} != {product} {overflow_string}")
+		print_io(N, multiplicand, multiplier, product, overflow)
+		exit(-1)
+
+	log.success(f"{multiplicand} * {multiplier} = {product} {overflow_string}")
+
+	if cycles != N:
+		log.error(f"Latency was {cycles} cycles instead of {N}")
+		exit(-1)
 
 async def sweep(dut, clock):
 	clock.reset()
 	N = int(dut.N)
-	VALUES = int(pow(2, N))
+	VALUES = 2 ** N
 
 	dut.i_reset <= 1
 	await clock.next()
@@ -37,26 +66,10 @@ async def sweep(dut, clock):
 				await clock.next(hold = True)   # hold to allow o_finished to change
 				cycles += 1
 
-			(expected_product, expected_overflow) = calculate_product(N, multiplicand, multiplier)
-			actual_product = dut.o_product.value.integer
-			actual_overflow = dut.o_overflow.value.integer
+			product = dut.o_product.value.integer
+			overflow = dut.o_overflow.value.integer
 
-			has_overflow = None
-			if actual_overflow == 1:
-				has_overflow = "with"
-			else:
-				has_overflow = "without"
-
-			if actual_product != expected_product or actual_overflow != expected_overflow:
-				log.error(f"{multiplicand} * {multiplier} != {actual_product} {has_overflow} overflow")
-				print_io(multiplicand, multiplier, actual_product, actual_overflow)
-				exit(-1)
-
-			log.success(f"{multiplicand} * {multiplier} = {actual_product} {has_overflow} overflow")
-
-			if cycles != N:
-				log.error(f"Latency was {cycles} cycles instead of {N}")
-				exit(-1)
+			verify(N, cycles, multiplicand, multiplier, product, overflow)
 
 	dut.i_start <= 0
 	await clock.next()

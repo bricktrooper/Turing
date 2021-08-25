c32d1e88f9b7e1055e70f0c7ba7a08f84fb43f8c
bricktrooper <kylepinto@yahoo.ca>
2021-08-24 21:31:46 -0400

    divider: Refactor testbench

--- testbenches/divider/divider_tb.py
+++ testbenches/divider/divider_tb.py
@@ -1,13 +1,75 @@
 import log
 import cocotb
 
-from math import pow, floor
+from math import floor
 from clock import Clock
+
+def predict_output(N, dividend, divisor):
+	if divisor == 0:
+		quotient = 0    # don't care
+		remainder = 0   # don't care
+		undefined = 1
+	else:
+		quotient = floor(dividend / divisor)
+		remainder = dividend % divisor
+		undefined = 0
+	return (quotient, remainder, undefined)
+
+def print_io(N, dividend, divisor, quotient, remainder, undefined):
+	log.info("============== I/O ==============")
+	log.info(f"N           : {N} b")
+	log.info(f"i_dividend  : {dividend}")
+	log.info(f"i_divisor   : {divisor}")
+	log.info(f"o_quotient  : {quotient}")
+	log.info(f"o_remainder : {remainder}")
+	log.info(f"o_undefined : {undefined}")
+	log.info("=================================")
+
+def verify(N, cycles, dividend, divisor, quotient, remainder, undefined):
+	(expected_quotient, expected_remainder, expected_undefined) = predict_output(N, dividend, divisor)
+
+	undefined_string = None
+	if undefined == 1:
+		undefined_string = "undefined"
+	else:
+		undefined_string = "defined"
+
+	# check divide by zero (undefined)
+	if undefined != expected_undefined:
+		log.error(f"{dividend} / {divisor} is not {undefined_string}")
+		log.error(f"{dividend} % {divisor} is not {undefined_string}")
+		print_io(N, dividend, divisor, quotient, remainder, undefined)
+		exit(-1)
+
+	if undefined:
+		log.success(f"{dividend} / {divisor} is {undefined_string}")
+		log.success(f"{dividend} % {divisor} is {undefined_string}")
+		return
+
+	# check quotient
+	if quotient != expected_quotient:
+		log.error(f"{dividend} / {divisor} != {quotient}")
+		print_io(N, dividend, divisor, quotient, remainder, undefined)
+		exit(-1)
+
+	log.success(f"{dividend} / {divisor} = {quotient}")
+
+	# check remainder (modulus)
+	if remainder !=expected_remainder:
+		log.error(f"{dividend} % {divisor} != {remainder}")
+		print_io(N, dividend, divisor, quotient, remainder, undefined)
+		exit(-1)
+
+	log.success(f"{dividend} % {divisor} = {remainder}")
+
+	if cycles != N:
+		log.error(f"Latency was {cycles} cycles instead of {N}")
+		exit(-1)
 
 async def sweep(dut, clock):
 	clock.reset()
 	N = int(dut.N)
-	VALUES = int(pow(2, N))
+	VALUES = 2 ** N
 
 	dut.i_reset <= 1
 	await clock.next()
@@ -24,51 +86,11 @@ async def sweep(dut, clock):
 				await clock.next(hold = True)   # hold to allow o_finished to change
 				cycles += 1
 
-			# check divide by zero (undefined)
 			undefined = dut.o_undefined.value.integer
+			quotient = dut.o_quotient.value.integer
+			remainder = dut.o_remainder.value.integer
 
-			if divisor == 0:
-				if undefined != 1:
-					log.error(f"Undefined was not asserted for divisor = {divisor}")
-					exit(-1)
-				else:
-					log.success(f"{dividend} / {divisor} = undefined")
-					log.success(f"{dividend} % {divisor} = undefined")
-					continue
-
-			if divisor != 0 and undefined != 0:
-				log.error(f"Undefined was asserted for divisor = {divisor}")
-				exit(-1)
-
-			# check quotient
-			expected = floor(dividend / divisor)
-			actual = dut.o_quotient.value.integer
-
-			if actual != expected:
-				log.error(f"{dividend} / {divisor} != {actual}")
-				log.info(f"dividend  : {dividend}")
-				log.info(f"divisor   : {divisor}")
-				log.info(f"quotient  : {actual}")
-				exit(-1)
-
-			log.success(f"{dividend} / {divisor} = {expected}")
-
-			# check remainder (modulus)
-			expected = dividend % divisor
-			actual = dut.o_remainder.value.integer
-
-			if actual != expected:
-				log.error(f"{dividend} % {divisor} != {actual}")
-				log.info(f"dividend  : {dividend}")
-				log.info(f"divisor   : {divisor}")
-				log.info(f"remainder : {actual}")
-				exit(-1)
-
-			log.success(f"{dividend} % {divisor} = {expected}")
-
-			if cycles != N:
-				log.error(f"Latency was {cycles} cycles instead of {N}")
-				exit(-1)
+			verify(N, cycles, dividend, divisor, quotient, remainder, undefined)
 
 	dut.i_start <= 0
 	await clock.next()

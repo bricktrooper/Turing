cdd616a5da07d12d7743c44494486da47a5e353b
bricktrooper <kylepinto@yahoo.ca>
2021-08-24 21:31:01 -0400

    subtractor: Refactor testbench

--- testbenches/subtractor/subtractor_tb.py
+++ testbenches/subtractor/subtractor_tb.py
@@ -1,21 +1,51 @@
 import log
 import cocotb
 
-from math import pow
 from clock import Clock
-from cocotb.binary import BinaryValue
 
-def join(difference, borrow):
-	BITS = difference.n_bits
-	value = BinaryValue(n_bits = BITS + 1,
-	                    bigEndian = False,
-						value = difference.integer + (borrow.integer << BITS))
-	return value.signed_integer
+def predict_output(N, minuend, subtrahend):
+	if minuend < subtrahend:
+		difference = (2 ** N) - (subtrahend - minuend)
+		borrow = 1
+	else:
+		difference = minuend - subtrahend
+		borrow = 0
+	return (difference, borrow)
+
+def print_io(N, minuend, subtrahend, difference, borrow):
+	log.info("============== I/O ==============")
+	log.info(f"N            : {N} b")
+	log.info(f"i_minuend    : {minuend}")
+	log.info(f"i_subtrahend : {subtrahend}")
+	log.info(f"o_difference : {difference}")
+	log.info(f"o_borrow     : {borrow}")
+	log.info("=================================")
+
+def verify(N, minuend, subtrahend, difference, borrow):
+	(expected_difference, expected_borrow) = predict_output(N, minuend, subtrahend)
+
+	borrow_string = None
+	if borrow == 1:
+		borrow_string = "with borrow"
+	else:
+		borrow_string = "without borrow"
+
+	if difference != expected_difference:
+		log.error(f"{minuend} - {subtrahend} != {difference} {borrow_string}")
+		print_io(N, minuend, subtrahend, difference, borrow)
+		exit(-1)
+
+	if borrow != expected_borrow:
+		log.error(f"{minuend} - {subtrahend} != {difference} {borrow_string}")
+		print_io(N, minuend, subtrahend, difference, borrow)
+		exit(-1)
+
+	log.success(f"{minuend} - {subtrahend} = {difference} {borrow_string}")
 
 async def sweep(dut, clock):
 	clock.reset()
 	N = int(dut.N)
-	VALUES = int(pow(2, N))
+	VALUES = 2 ** N
 
 	for minuend in range(VALUES):
 		for subtrahend in range(VALUES):
@@ -23,21 +53,10 @@ async def sweep(dut, clock):
 			dut.i_subtrahend <= subtrahend
 			await clock.next()
 
-			expected = minuend - subtrahend
-			actual = join(dut.o_difference.value, dut.o_borrow.value)
-
 			difference = dut.o_difference.value.integer
 			borrow = dut.o_borrow.value.integer
 
-			if actual != expected:
-				log.error(f"{minuend} - {subtrahend} != {actual}")
-				log.info(f"minuend    : {minuend}")
-				log.info(f"subtrahend : {subtrahend}")
-				log.info(f"difference : {difference}")
-				log.info(f"borrow     : {borrow}")
-				exit(-1)
-
-			log.success(f"{minuend} - {subtrahend} = {expected}")
+			verify(N, minuend, subtrahend, difference, borrow)
 
 @cocotb.test()
 async def testbench(dut):
